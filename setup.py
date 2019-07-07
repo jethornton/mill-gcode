ef0b58878024afc34ad51bf247f693549b1479d2
John Thornton <jthornton@gnipsel.com>
2019-07-07 06:27:13 -0500

    work on setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,9 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-# list of (destination, source_file) tuples
-DATA_FILES = [('~/.local/bin', ['m7i96/mesaflash64', 'm7i96/mesaflash32',])]
-data_files = [(os.path.expanduser(dest), src_list) for dest, src_list in DATA_FILES]
 
 setup(
-    name="7i96",
+    name="Mill G code",
     version="0.1.2",
     author="John Thornton",
     author_email="<jt@gnipsel.com>",
@@ -20,7 +17,6 @@ setup(
     download_url="https://github.com/jethornton/mct7i96/tarball/master",
     python_requires='>=2',
     packages=find_packages(),
-    data_files=data_files,
     include_package_data=True,
     entry_points={
         'gui_scripts': ['mill-gcode=mill-gcode.mgc:main',],
